abc4dce1c442cee63c95a90e21b5dc69c48c691f
a-mahir-d <amd.coding.ac@gmail.com>
2026-04-08 17:38:41 +0200

    confs updated

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "AMD Article",
     pageTitleSuffix: "",
-    enableSPA: false,
+    enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
