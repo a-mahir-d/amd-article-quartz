36c60d0b9264eafe69fa712f757e8285ba6e517f
a-mahir-d <amd.coding.ac@gmail.com>
2026-04-08 17:16:13 +0200

    url fixed

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "ctf.ahmetmahirdemirelli.com",
+    baseUrl: "article.ahmetmahirdemirelli.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
